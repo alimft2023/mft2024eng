524e4c3cfb3926b590c2addea5833da4bf485d29
aliasgharsalehi <aliideno2010@gmail.com>
2024-03-24 00:16:42 +0330

    'init'

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import UserProfile
+from .models import UserProfile,Education,Experience
 
 # Register your models here.
 admin.site.register(UserProfile)
+admin.site.register(Education)
+admin.site.register(Experience)
