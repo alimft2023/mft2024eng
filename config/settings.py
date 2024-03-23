0038e229472948644a6193ec801d113921e85b27
aliasgharsalehi <aliideno2010@gmail.com>
2024-03-23 10:50:35 +0330

    'init'

--- config/settings.py
+++ config/settings.py
@@ -118,8 +118,14 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
-
+MEDIA_URL='media/'
+MEDIA_ROOT=[
+    BASE_DIR / "media",
+]
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+
+
--- config/urls.py
+++ config/urls.py
@@ -14,11 +14,14 @@ Including another URLconf
     1. Import the include() function: from django.urls import include, path
     2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
 """
+
 from django.contrib import admin
 from django.urls import include, path
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
-    path('admin/', admin.site.urls),
-    path('blog/',include('blog.urls')),
-    path('accounts/',include('accounts.urls')),
-]
+    path("admin/", admin.site.urls),
+    path("blog/", include("blog.urls")),
+    path("accounts/", include("accounts.urls")),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
